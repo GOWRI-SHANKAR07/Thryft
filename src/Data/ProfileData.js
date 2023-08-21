abf7dca580ce5d26a870902774859db7a026a794
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-21 19:36:26 +0530

    Data: Profile Data: Data for rendering profile screen content: Completed

---
+++ src/Data/ProfileData.js
@@ -0,0 +1,95 @@
+export const ProfileDataCont1 = [
+    {
+        iconName: 'wallet-outline',
+        title: 'Wallet',
+        IDR: 'IDR 300.000',
+        mode: '',
+        divider: true,
+        version: '',
+    },
+
+    {
+        iconName: 'ios-cart-outline',
+        title: 'My Order',
+        IDR: '',
+        mode: '',
+        divider: true,
+        version: '',
+    },
+
+    {
+        iconName: 'reader-outline',
+        title: 'Manage Catalog',
+        IDR: '',
+        mode: '',
+        divider: true,
+        version: '',
+    },
+
+    {
+        iconName: 'arrow-redo-outline',
+        title: 'Share Catalog',
+        IDR: '',
+        mode: '',
+        divider: true,
+        version: '',
+    },
+
+    {
+        iconName: 'tennisball-outline',
+        title: 'Vacation Mode',
+        IDR: '',
+        mode: '',
+        divider: false,
+        version: '',
+    }
+]
+
+export const ProfileDataCont2 = [
+    {
+        iconName: 'chatbubbles-outline',
+        title: 'Forum',
+        IDR: '',
+        mode: '',
+        divider: true,
+        version: '',
+    },
+
+    {
+        iconName: 'settings-outline',
+        title: 'Settings',
+        IDR: '',
+        mode: '',
+        divider: false,
+        version: '',
+    }
+]
+
+export const ProfileDataCont3 = [
+    {
+        iconName: 'star-outline',
+        title: 'Rate our application',
+        IDR: '',
+        mode: '',
+        divider: true,
+        version: '',
+    },
+
+    {
+        iconName: 'information-circle-outline',
+        title: 'About Us',
+        IDR: '',
+        mode: '',
+        divider: true,
+        version: '',
+    },
+
+    {
+        iconName: 'log-out-outline',
+        title: 'Log Out',
+        IDR: '',
+        mode: '',
+        divider: false,
+        version: 'v4.3',
+    }
+]
