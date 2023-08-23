1a6917c72912083fd208a24732d12c5d60327f07
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-23 18:24:46 +0530

    Feat: Profile upload Context: Added

--- src/Context/ContextProvider.js
+++ src/Context/ContextProvider.js
@@ -4,20 +4,23 @@ import React, { createContext, useContext, useMemo, useState } from 'react'
 const AppContext = createContext();
 
 export const useAppContext = () => {
-    return useContext(AppContext);
+  return useContext(AppContext);
 }
 
 const ContextProvider = ({ children }) => {
+  // tab bar screen switch handling state
+  const [profile, setProfile] = useState(false);
+  // profile pic permisiion  & upload state
+  const [permissionGranted, setPermissionGranted] = useState(false);
+  const [imageUri, setImageUri] = useState(null);
 
-    const [profile, setProfile] = useState(false);
-
-    const isProfile = useMemo(() => {
-        return { profile, setProfile };
-    }, [profile])
+  const isProfile = useMemo(() => {
+    return { profile, setProfile, permissionGranted, setPermissionGranted, imageUri, setImageUri };
+  }, [profile, permissionGranted, imageUri])
 
   return (
-    <AppContext.Provider value={ isProfile }>
-        {children}
+    <AppContext.Provider value={isProfile}>
+      {children}
     </AppContext.Provider>
   )
 }
