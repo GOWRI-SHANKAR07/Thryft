7250bc1f1ac8bc3ee1ea0d4e27b67019ce7fa0e3
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-23 18:23:57 +0530

    Feat: Custom Font Implementation: Improgress

--- App.js
+++ App.js
@@ -1,18 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect } from 'react';
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import StackScreens from './src/Stack/StackScreens';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-      <StackScreens />
-  );
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+  });
+
+  useEffect(()=> {
+    if(fontsLoaded){
+      onLayoutRootView();
+    }
+  }, [fontsLoaded])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  } else {
+    return (
+        <StackScreens />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
