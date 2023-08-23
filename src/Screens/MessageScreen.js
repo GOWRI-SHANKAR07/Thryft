e0e26f87c2c1ab7a8295577ec25e6e1bfd48323e
Gowrish_22 <gowrishankar29102002@gmail.com>
2023-08-23 18:25:13 +0530

    Test: Profile upload setup code test: Tested

--- src/Screens/MessageScreen.js
+++ src/Screens/MessageScreen.js
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {
+  Button,
+  PermissionsAndroid,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-const MessageScreen = () => {
-  return (
-    <View>
-      <Text>MessageScreen</Text>
-    </View>
-  )
-}
+const requestCameraPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: 'Cool Photo App Camera Permission',
+        message:
+          'Cool Photo App needs access to your camera ' +
+          'so you can take awesome pictures.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the camera');
+    } else {
+      console.log('Camera permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
-export default MessageScreen
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.item}>Try permissions</Text>
+    <Button title="request permissions" onPress={requestCameraPermission} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  item: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default App;
