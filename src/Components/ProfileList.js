12e104f32026d7fe1f8a7028b94855b89c59e84a
Gowrish_22 <91775141+GOWRI-SHANKAR07@users.noreply.github.com>
2023-08-25 17:56:15 +0530

    Updated: Design: Updated

--- src/Components/ProfileList.js
+++ src/Components/ProfileList.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
@@ -10,37 +10,39 @@ const ProfileList = ({ items }) => {
 
     return (
         <View>
-            <View style={styles.profileCont}>
-                <View style={styles.inner1Cont}>
-                    <Ionicons
-                        style={{ paddingLeft: pixelSizeHorizontal(10) }}
-                        name={items.iconName}
-                        size={24}
-                        color= {items.iconName === 'log-out-outline' ? Colors.red : Colors.black}
-                    />
-                    <Text style={[styles.profileTxt, { color: items.iconName === 'log-out-outline' ? Colors.red : Colors.black }]}>{items.title}</Text>
-                </View>
-                <View style={styles.inner1Cont}>
-                    <Text style={{ paddingRight: pixelSizeHorizontal(10) }}>
-                        {items.IDR != '' ? items.IDR : null}
-                    </Text>
-                    <Text style={{ paddingRight: pixelSizeHorizontal(10) }}>
-                        {items.mode != '' ? items.mode : null}
-                    </Text>
-                    {items.version != '' ?
-                        <Text>{items.version}</Text>
-                        :
-                        < Entypo
-                            name="chevron-right"
+            <TouchableOpacity>
+                <View style={styles.profileCont}>
+                    <View style={styles.inner1Cont}>
+                        <Ionicons
+                            style={{ paddingLeft: pixelSizeHorizontal(10) }}
+                            name={items.iconName}
                             size={24}
-                            color={Colors.black}
+                            color={items.iconName === 'log-out-outline' ? Colors.red : Colors.black}
                         />
-                    }
+                        <Text style={[styles.profileTxt, { color: items.iconName === 'log-out-outline' ? Colors.red : Colors.black }]}>{items.title}</Text>
+                    </View>
+                    <View style={styles.inner1Cont}>
+                        <Text style={{ paddingRight: pixelSizeHorizontal(10) }}>
+                            {items.IDR != '' ? items.IDR : null}
+                        </Text>
+                        <Text style={{ paddingRight: pixelSizeHorizontal(10) }}>
+                            {items.mode != '' ? items.mode : null}
+                        </Text>
+                        {items.version != '' ?
+                            <Text>{items.version}</Text>
+                            :
+                            < Entypo
+                                name="chevron-right"
+                                size={24}
+                                color={Colors.black}
+                            />
+                        }
+                    </View>
                 </View>
-            </View>
-            {
-                items.divider && <View style={styles.divLine}></View>
-            }
+                {
+                    items.divider && <View style={styles.divLine}></View>
+                }
+            </TouchableOpacity>
         </View>
     )
 }
