c8ddbcdf4dec6559d5a28c34230e910bf3766ed6
Gowrish_22 <91775141+GOWRI-SHANKAR07@users.noreply.github.com>
2023-08-25 18:04:52 +0530

    Initial commit

---
+++ src/Screens/OrderedItems.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OrderedItems = () => {
+    
+  return (
+    <View>
+      <Text>OrderedItems</Text>
+    </View>
+  )
+}
+
+export default OrderedItems
