4c4d2fe1022da153ee0329465fbcbffa9770bf20
Gowrish_22 <91775141+GOWRI-SHANKAR07@users.noreply.github.com>
2023-08-25 18:00:24 +0530

    Feat: Screens Added

--- src/Stack/StackScreens.js
+++ src/Stack/StackScreens.js
@@ -1,30 +1,48 @@
 import { View, Text, SafeAreaView, StatusBar } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../Screens/HomeScreen';
 import TabScreens from './TabScreens';
 import { ColorSchemeProvider } from '../Theme/ColorTheme';
 import ContextProvider from '../Context/ContextProvider';
+import LoginScreen from '../Screens/LoginScreen';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 // creating stack
 const Stack = createNativeStackNavigator();
 
 const StackScreens = () => {
+
+    //user state
+    const [user, setUser] = useState(null)
+    const auth = getAuth();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            setUser(user)
+            console.log(user)
+
+        })
+    }, [])
+
     return (
-            <ContextProvider>
-                <ColorSchemeProvider>
-                    <NavigationContainer>
-                        <Stack.Navigator
-                            screenOptions={{
-                                headerShown: false
-                            }}
-                        >
-                            <Stack.Screen name='Tab' component={TabScreens} />
-                        </Stack.Navigator>
-                    </NavigationContainer>
-                </ColorSchemeProvider>
-            </ContextProvider>
+        <ContextProvider>
+            <ColorSchemeProvider>
+                <NavigationContainer>
+                    <Stack.Navigator
+                        screenOptions={{
+                            headerShown: false
+                        }}
+                        initialRouteName={'Login'}
+                    >
+                        <Stack.Screen name='Login' component={LoginScreen} />
+                        <Stack.Screen name='Tab' component={TabScreens} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </ColorSchemeProvider>
+        </ContextProvider>
     )
 }
 
