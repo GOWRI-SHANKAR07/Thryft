2f23d00fba6ffaf772e8feaaee9fd30c492a9b71
Gowrish_22 <91775141+GOWRI-SHANKAR07@users.noreply.github.com>
2023-08-25 17:58:56 +0530

    Initial commit

--- src/Screens/OrderScreen.js
+++ src/Screens/OrderScreen.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, Animated } from 'react-native'
 import React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import OrderedItems from './OrderedItems';
+import OrderHistory from './OrderHistory';
+import CustomerSupport from './CustomerSupport';
+import { pixelSizeVertical } from '../Constants/Theme';
+import { styles } from '../styles/Orders';
+
+const Tab = createMaterialTopTabNavigator();
 
 const OrderScreen = () => {
   return (
-    <View>
-      <Text>OrderScreen</Text>
-    </View>
-  )
+    <Tab.Navigator style={styles.container}>
+      <Tab.Screen name='Items' component={OrderedItems} />
+      <Tab.Screen name='History' component={OrderHistory} />
+      <Tab.Screen name='Support' component={CustomerSupport} />
+    </Tab.Navigator >
+  );
 }
+
+
+
 
 export default OrderScreen
