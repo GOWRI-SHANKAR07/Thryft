532a0d067fb2127b0280744dee4b021993fcd570
Gowrish_22 <91775141+GOWRI-SHANKAR07@users.noreply.github.com>
2023-08-31 17:35:13 +0530

    Feat: OrderedItems: Ordered items of the user UI: Completed

--- src/Screens/OrderedItems.js
+++ src/Screens/OrderedItems.js
@@ -1,17 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Svg, { Circle, Rect } from 'react-native-svg'
 import { Colors } from '../Constants/Theme'
 import { styles } from '../styles/Items'
+import { AntDesign, Ionicons } from '@expo/vector-icons'
+import ItemList from '../Components/ItemList'
+import { orderedItems } from '../Data/ItemsData'
 
 const OrderedItems = () => {
 
   return (
     <View style={styles.container}>
-      <Svg height="50%" width="50%" viewBox='0 0 110 110' >
-        <Circle cx="50" cy="50" r="50" stroke="#000" strokeWidth="2" fill={Colors.primary} />
-        <Rect x="15" y="15" width="70" height="70" stroke="red" strokeWidth="2" fill="yellow" />
-      </Svg>
+      <View style={styles.searchCont}>
+        <View style={styles.searchBox}>
+          <AntDesign
+            name="search1"
+            size={24}
+            color="black"
+            style={styles.search}
+          />
+          <TextInput
+            style={styles.searchTxt}
+            placeholder='Search your order'
+          />
+        </View>
+        <TouchableOpacity>
+          <View style={styles.filterCont}>
+            <Ionicons
+              name="filter-sharp"
+              size={24}
+              color="black"
+            />
+            <Text style={styles.filterTxt}>Filters</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.dividerLine} />
+      {orderedItems.map((item) => <ItemList key={item.id} items={item} index={item.id} />)}
     </View>
   )
 }
